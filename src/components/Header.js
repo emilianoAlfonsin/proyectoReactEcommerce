3f6d271b936e464791963ab92a76d4e0118fa63c
EmilianoAlfonsin <emiliano.a.83@gmail.com>
2022-08-22 00:46:15 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import './Header.css'
+import logoWhite from '../assets/logo-white.png'
+// import a from '@mui/material/Button';
+
+
+export const Header = () => {
+    return (
+        <div className="header-container">
+            <div className="header">
+                <div className="header-logo">
+                    <img className="header-logo" src={logoWhite} alt="logo"/>
+                </div>
+                <div className="navbar">
+                    <a className="navlink" href="#" >Home</a>
+                    <a className="navlink" href="#" >Catalogo</a>
+                    <a className="navlink" href="#" >Quienes somos</a>
+                    <a className="navlink" href="#" >Contacto</a>
+                </div>
+            </div>
+        </div>
+    )
+}
